bbaeb4711497e385028699222e16df674250ec6c
Dyanna <dyanna.person@gmail.com>
2022-11-21 14:08:39 -0500

    forms and html

--- app/forms.py
+++ app/forms.py
@@ -3,26 +3,46 @@ from wtforms import StringField, PasswordField, SubmitField , BooleanField
 from wtforms.validators import DataRequired, EqualTo
 
 
-# class SignUpForm(FlaskForm):
-#     email = StringField('Email', validators=[DataRequired()])
-#     username = StringField('Username', validators=[DataRequired()])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-#     submit = SubmitField()
+class SignUpForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField()
 
-# class LogInForm(FlaskForm):
-#     username = StringField('Username', validators=[DataRequired()])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     submit = SubmitField()
+class LogInForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    #remember_me = BooleanField('Remember Me')
+    submit = SubmitField()
 
-# class PostForm(FlaskForm):
-#     title = StringField('Title', validators=[DataRequired()])
-#     body = StringField('Body', validators=[DataRequired()])
-#     submit = SubmitField()
+class ReferralScreeningForm(FlaskForm):
+    patient_name = StringField('Patient Full Name', validators=[DataRequired()])
+    question1 = BooleanField('Does the Patient have a history of breast cancer at age ≤50')
+    question2 = BooleanField('Does the Patient have a history of ovarian cancer at any age')
+    question3 = BooleanField('Does the patients mother have a history of breast cancer at age ≤50')
+    question4 = BooleanField('Does the patients mother have a history of ovarian cancer at any age')
+    question5 = BooleanField('Does the patients sister have a history of breast cancer at age ≤50')
+    question6 = BooleanField('Does the patients sister have a history of ovarian cancer at any age')
+    question7 = BooleanField('Does the patients daughter have a history of breast cancer at age ≤50')
+    question8 = BooleanField('Does the patients daughter have a history of ovarian cancer at any age')
+    question9 = BooleanField('Does the patients maternal grandmother have a history of breast cancer at age ≤50')
+    question10 = BooleanField('Does the patients maternal grandmother have a history of ovarian cancer at any age')
+    question11 = BooleanField('Does the patients maternal aunt have a history of breast cancer at age ≤50')
+    question12 = BooleanField('Does the patients maternal aunt have a history of ovarian cancer at any age')
+    question13 = BooleanField('Does the patients paternal grandmother have a history of breast cancer at age ≤50')
+    question14 = BooleanField('Does the patients paternal grandmother have a history of ovarian cancer at any age')
+    question15= BooleanField('Does the patients paternal aunt have a history of breast cancer at age ≤50')
+    question16= BooleanField('Does the patients paternal aunt have a history of ovarian cancer at any age')
+    question17 = BooleanField('Does the Patient ≥ 2 cases of breast cancer after age 50 on same side of family')
+    question18 = BooleanField('Does the Patient have a family history of male breast cancer')
+    question19 = BooleanField('Is the patient of Jewish Ancestry')
+    question20 = BooleanField('Does the patients mother have a history of ovarian cancer at any age')
+    question21 = BooleanField('Does the Patient have a history of breast cancer at age 50')
+    question22 = BooleanField('Does the Patient have a history of ovarian cancer at any age')
+    question23 = BooleanField('Does the patients mother have a history of breast cancer at age ≤50')
+    question24 = BooleanField('Does the patients mother have a history of ovarian cancer at any age')
+    submit = SubmitField()
 
-# class ReferralTool(FlaskForm):
-#     email = StringField('Email', validators=[DataRequired()])
-#     username = StringField('Username', validators=[DataRequired()])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-#     submit = SubmitField()
+    
+
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,82 @@
+# from app import app
+# from flask import render_template, flash, redirect, url_for
+# from app.forms import SignUpForm, LogInForm
+# # from app.models import User, Post
+
+
+
+from flask import render_template, redirect, url_for, flash
 from app import app
-from flask import render_template
+from app.forms import SignUpForm, LogInForm, ReferralScreeningForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
     return render_template('index.html', title='Home', user=user)
+    
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        print('You Are Officially Signed Up')
+        # Get data from form
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(email, username, password)
+        # # Check to see if we have a user with username and/or password:
+        # check_user = User.query.filter( (User.username == username) | (User.email == email) ).first()
+        # if check_user is not None:
+        #     flash('User with username and/or email already exists', 'danger')
+        #     return redirect(url_for('signup'))
+        # # Add a new user to the database
+        # new_user = User(email=email, username=username, password=password)
+        # # Flash a success message
+        # flash(f"{new_user} has successfully signed up!", "success")
+        # Redirect back to home
+        # return redirect(url_for('index'))
+
+    return render_template('signup.html', form=form)
+
+
+
+@app.route('/login')
+def login():
+    form = LogInForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        # return redirect('/index')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     form = LogInForm()
+#     if form.validate_on_submit():
+#         # Get the form data
+#         username = form.username.data
+#         password = form.password.data
+
+        # Check to see if there is a user with that username and password
+        # user = User.query.filter_by(username=username).first()
+        # if user is not None and user.check_password(password):
+        #     # log the user in
+        #     login_user(user)
+        #     flash(f"{user} is now logged in.", 'primary')
+        #     return redirect(url_for('index'))
+        # else:
+        #     flash('Incorrect username and/or password. Please try again.', 'danger')
+        #     return redirect(url_for('login'))
+
+    # return render_template('login.html', form=form)
+
+@app.route('/screening', methods=['GET', 'POST'])
+def screening():
+    form = ReferralScreeningForm()
+    return render_template('screeningtool.html', title = 'Referral Screening Tool', form=form)
+   
     
