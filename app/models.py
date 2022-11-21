55ad77417d08ec7dc914c2045c8a65acfa733b81
Dyanna <dyanna.person@gmail.com>
2022-11-21 17:36:23 -0500

    Form Entries Not Showing Up

--- app/models.py
+++ app/models.py
@@ -1,10 +1,29 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+# from flask_login import UserMixin
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(256), nullable=False)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)    
+    # def __repr__(self):
+    #     return '<User {}>'.format(self.username)    
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Set the password to the hashed version of the password
+        self.password = self.set_password(kwargs.get('password', ''))
+        # Add and commit the new instance to the database
+        db.session.add(self)
+        db.session.commit()
+
+    def __str__(self):
+        return self.username
+
+    def set_password(self, plain_password):
+        return generate_password_hash(plain_password)
+
+    def check_password(self, password_guess):
+        return check_password_hash(self.password, password_guess)
