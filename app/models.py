e65f465965aef4cf0ce2d4b71a0431ab0d6e9d97
Dyanna <dyanna.person@gmail.com>
2022-11-23 04:03:08 -0500

    add relationship

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), index=True, unique=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(256), nullable=False)
+    tool = db.relationship('Tool', backref='clinician', lazy='dynamic')
 
    
     
