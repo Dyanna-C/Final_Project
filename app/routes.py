5e6f0597ac59ddcc4f87f0352d7e74cbd71717a1
Dyanna <dyanna.person@gmail.com>
2022-11-23 14:05:36 -0500

    afternoon

--- app/routes.py
+++ app/routes.py
@@ -105,7 +105,7 @@ def screening():
         question24= form.question24.data
 
         # Create a new instance of Post with the info from the form
-        new_tool = Tool(firstname = firstname, lastname= lastname, question1 = question1, question2=question2, question3 = question3, 
+        fresh_tool = Tool(firstname = firstname, lastname= lastname, question1 = question1, question2=question2, question3 = question3, 
         question4 = question4, question5 = question5, question6 = question6, question7 = question7, question8 = question8,
         question9 = question9, question10 = question10, question11 = question11, question12 = question12, question13 = question13,
         question14 = question14, question15 = question15, question16=question16, question17 = question17, 
@@ -114,8 +114,8 @@ def screening():
         question24 = question24, user_id=current_user.id)
         
         # flash a message of success
-        flash(f"Screening for {new_tool.firstname}{new_tool.lastname} is complete.", "success")
-        # Redirect back to the home page
+        flash(f"Screening for patient # {fresh_tool.firstname}{fresh_tool.lastname} is complete.", "success")
+        # Redirect back to the home pag
         return redirect(url_for('index'))
 
     return render_template('screeningtool.html', form=form)
@@ -123,12 +123,73 @@ def screening():
     #     return redirect(url_for('index'))
     # return render_template('screeningtool.html', title = 'Referral Screening Tool', form=form)
 
+@app.route('/posts/<tool_id>')
+@login_required
+def get_patient(tool_id):
+    form = ReferralScreeningForm()
+    tool = Tool.query.get(tool_id)
+    if not tool:
+        flash(f"No Patient with id #{tool_id} has been screened. Please complete their screening ASAP", "warning")
+        return redirect(url_for('index'))
+    return render_template('screeningtool.html', form = form)
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = LogInForm()
-#     if form.validate_on_submit():
-#         username = form.username.data
-#         password = form.password.data
-#         return redirect(url_for('index'))
-#     return render_template('login.html', title='Sign In', form=form)
+@app.route('/posts/<tool_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_patient(tool_id):
+    form = ReferralScreeningForm()
+    tool = Tool.query.get(tool_id)
+    if not tool:
+        flash(f"No Patient with id #{tool_id} has been screened", "warning")
+        return redirect(url_for('index'))
+    # if post.author != current_user:
+    #     flash('You do not have permission to edit this post', 'danger')
+    #     return redirect(url_for('index'))
+    form = ReferralScreeningForm()
+    if form.validate_on_submit():
+        # Get the form data
+        new_firstname = form.firstname.data
+        new_lastname = form.lastname.data
+        new_question1 = form.question1.data
+        new_question2 = form.question2.data
+        new_question3 = form.question3.data
+        new_question4 = form.question4.data
+        new_question5 = form.question5.data
+        new_question6 = form.question6.data
+        new_question7 = form.question7.data
+        new_question8 = form.question8.data
+        new_question9 = form.question9.data
+        new_question10 = form.question10.data
+        new_question11 = form.question11.data
+        new_question12 = form.question12.data
+        new_question13 = form.question13.data
+        new_question14 = form.question14.data
+        new_question15 = form.question15.data
+        new_question16 = form.question16.data
+        new_question17 = form.question17.data
+        new_question18 = form.question18.data
+        new_question19 = form.question19.data
+        new_question20 = form.question20.data
+        new_question21 = form.question21.data
+        new_question22 = form.question22.data
+        new_question23= form.question23.data
+        new_question24= form.question24.data
+        # update the tool
+        flash(f"You have updated the Screening Results for Patient {tool.firstname}{tool.lastname}  ", "success")
+
+
+        Tool(firstname = new_firstname, lastname= new_lastname, question1 =new_question1, question2=new_question2, 
+        question3 = new_question3, 
+        question4 = new_question4, question5 = new_question5, question6 = new_question6, question7 = new_question7, 
+        question8 = new_question8,
+        question9 = new_question9, question10 = new_question10, question11 = new_question11, question12 = new_question12,
+        question13 = new_question13,
+        question14 = new_question14, question15 = new_question15, question16=new_question16, question17 = new_question17, 
+        question18 = new_question18, question19 = new_question19, question20 = new_question20, question21 = new_question21, 
+        question22 = new_question22, question23 = new_question23,
+        question24 = new_question24, user_id=current_user.id)
+        flash(f"You have updated the Screening Results for Patient {tool.firstname}{tool.lastname}  ", "success")
+        return redirect(url_for('index', tool_id=tool.id))
+    return render_template('edit_post.html', post=tool, form=form)
+
+
+
